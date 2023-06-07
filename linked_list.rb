c2435b3452cbd0f882f4246de26333bbecb61ba4
matterramun <Veradux4@protonmail.com>
2023-06-07 17:59:36 -0400

    Implement test to see if linked list contains a value.

--- linked_list.rb
+++ linked_list.rb
@@ -38,6 +38,31 @@ class List
       @head = new_node
     end
   end
+
+  def size
+    i = 0
+    current_node = @head
+    puts @head
+    until current_node.nil?
+      i += 1
+      current_node = current_node.next_node
+    end
+    puts "Depth of #{i}"
+    i
+  end
+
+  def contains?(target)
+    current_node = @head
+    loop do
+      if current_node.data == target
+        return true
+      elsif current_node.next_node.nil?
+        return false
+      else
+        current_node = current_node.next_node
+      end
+    end
+  end
 end
 
 test_list = List.new
@@ -47,10 +72,10 @@ test_list.append('test3')
 
 test_list.prepend('test4')
 p test_list
-=begin 
 p test_list.size
-puts test.contains?('test5')
-puts test.contains?('test3')
+puts test_list.contains?('test5')
+puts test_list.contains?('test3')
+=begin 
 p test.find('test2')
 p test.find('test5')
 test.pop
